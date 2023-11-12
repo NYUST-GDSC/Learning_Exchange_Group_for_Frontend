f5b84bbb4b4eb4a42fc48adf7438e0c10744c11d
charles <abcd854884@gmail.com>
2023-11-12 22:28:06 +0800

    🎉 init: 創建專案

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true, // 在語句末尾使用分號
+  singleQuote: true, // 使用單引號而不是雙引號
+  trailingComma: "all", // 在物件和數組最後一個元素後使用逗號
+  printWidth: 80, // 每行最大字符數
+  tabWidth: 2, // 縮進使用兩個空格
+  endOfLine: "lf", // 使用 Linux 系統預設的 LF 換行符
+  arrowParens: "avoid", // 當只有一個參數時，省略箭頭函數的括號
+};
+
+export default config;
